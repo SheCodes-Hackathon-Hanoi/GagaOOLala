a97dbd1e6f8b4e817781cacba2b98e4ec280154f
ctpanh <ctpanh03@gmail.com>
2023-09-30 16:14:27 +0700

    api list trending

--- server/travel/views.py
+++ server/travel/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.db.models import Count
+from travel.models import TravelList, Booking
 
 # Create your views here.
+@api_view(['GET'])
+def listTrending():
+    most_booked_travels = TravelList.objects.annotate(booking_count=Count('booking')).order_by('-booking_count')[:5]
+    travels = []
+    for travel in most_booked_travels:
+        if travel:
+            travels.append(travel)
+    return Response(travels)
