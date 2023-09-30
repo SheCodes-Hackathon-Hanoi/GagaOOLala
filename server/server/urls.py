badfdc5fd545e64587c4975e1ea38a3371ef3233
ctpanh <ctpanh03@gmail.com>
2023-09-30 19:01:54 +0700

    serializers model

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,9 +20,14 @@ from travel import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'travel', views.TravelList, 'travel')
+router.register(r"travel", views.TravelListView, basename='travel')
+router.register(r"user", views.CustomUserView, basename='user')
+router.register(r"booking", views.BookingView, basename='booking')
+router.register(r"favor", views.FavorView, basename='favor')
+router.register(r"review", views.ReviewView, basename='review')
 
 urlpatterns = [
     path("api/trending", views.listTrending),
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
+    # path("api/instances/", views.TravelListView.as_view(), name="instances"),
 ]
